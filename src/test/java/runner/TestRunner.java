fade1fe2102ef080913e78e6288c4cd42c04cb8f
Emrah Bulut <52751642+bltemrah@users.noreply.github.com>
2022-09-20 09:45:23 +0100

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features/roverNavigation.feature",
         glue = {"steps", "hooks"},
         plugin = {"pretty",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "html:build/html-reports.html"
         },
         tags = "",
